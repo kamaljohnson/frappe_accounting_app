ba0369ca9ecb67a2b50d94d2a567d855176ce09c
Kamal Johnson <kamaljohnson12345@gmail.com>
2021-06-25 09:40:57 +0530

    feat: Add customer doctype feat: Add supplier doctype

---
+++ frappe_accounting_app/frappe_accounting_app/doctype/customer/customer.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Kamal Johnson and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer', {
+	// refresh: function(frm) {
+
+	// }
+});
