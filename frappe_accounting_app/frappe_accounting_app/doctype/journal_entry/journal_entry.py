de7391cca0bc9b65d41fc9d70eac9e3be4c1e65f
kamaljohnson <kamaljohnson12345@gmail.com>
2021-07-07 10:48:10 +0530

    feat: Add journal entry before_save checks  - Check if total_credit == total_debit  - Update total_credits and total_debits
    fix: Set default credit and debit = 0

--- frappe_accounting_app/frappe_accounting_app/doctype/journal_entry/journal_entry.py
+++ frappe_accounting_app/frappe_accounting_app/doctype/journal_entry/journal_entry.py
@@ -1,8 +1,29 @@
 # Copyright (c) 2021, Kamal Johnson and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class JournalEntry(Document):
-	pass
+
+
+	def before_save(self):
+		""" 
+		1. Checks if total credits is equal to total debits | throws error msg 
+		2. Updates the total_credits and total_debits
+		"""
+		
+		self.total_debit = 0
+		self.total_credit = 0
+		
+		for accounting_entry in self.get('accounting_entries'):
+			self.total_credit += accounting_entry.credit
+			self.total_debit += accounting_entry.debit
+
+		if self.total_credit != self.total_debit:
+			frappe.throw(_('Total credit should be equal to total debit'))
+
+	# TODO: on_submit(): check if total_debt = total_credits
+	def on_submit(self):
+		pass
