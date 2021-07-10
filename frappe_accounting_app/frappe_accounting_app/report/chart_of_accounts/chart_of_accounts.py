be4c422f3e52a9ea5536489c0db7651917b598db
kamaljohnson <kamaljohnson12345@gmail.com>
2021-07-10 11:00:24 +0530

    feat: Init chart of accounts report

---
+++ frappe_accounting_app/frappe_accounting_app/report/chart_of_accounts/chart_of_accounts.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2013, Kamal Johnson and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+
+	"""
+	Chart of Accounts report Layout
+	
+	Account			Credit	Debit
+	..				..		..
+	
+	"""
+
+	return columns, data
