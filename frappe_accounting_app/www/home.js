fd1689e685f631456cd380e6de25d25d2bce274f
kamaljohnson <kamaljohnson12345@gmail.com>
2021-07-13 19:09:06 +0530

    feat: Add catalog button, improve ui

---
+++ frappe_accounting_app/www/home.js
@@ -0,0 +1,1 @@
+// TODO: add on searchbar input text change trigger search
--- frappe_accounting_app/www/home.py
+++ frappe_accounting_app/www/home.py
@@ -3,3 +3,6 @@ import frappe
 def get_context(context):
     context.company_name = frappe.get_all('Company')[0]['name']
     return context
+
+def search(text):
+    print('searching for {}'.format(text))
