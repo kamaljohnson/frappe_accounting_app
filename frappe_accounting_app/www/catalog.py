c545d200f3f973410055d26e3c7841e10c2462f3
kamaljohnson <kamaljohnson12345@gmail.com>
2021-07-13 20:46:51 +0530

    feat: Add catalog list view

--- frappe_accounting_app/www/catalog.py
+++ frappe_accounting_app/www/catalog.py
@@ -2,4 +2,5 @@ import frappe
 
 def get_context(context):
     context.company_name = frappe.get_all('Company')[0]['name']
+    context.items = frappe.get_all('Item', fields=['image', 'name', 'standard_selling_rate'])
     return context
