3eb035607f5a0e9b18a496527c5d8aa75a6db578
kamaljohnson <kamaljohnson12345@gmail.com>
2021-07-14 05:30:38 +0530

    feat: Display results in home

--- frappe_accounting_app/www/home.py
+++ frappe_accounting_app/www/home.py
@@ -2,6 +2,7 @@ import frappe
 
 def get_context(context):
     context.company_name = frappe.get_all('Company')[0]['name']
+    context.result_items = frappe.get_all('Item', fields=['image', 'name', 'standard_selling_rate'])
     return context
 
 def search(text):
