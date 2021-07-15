0e31680c6991831c5132944ce238b4867dffb370
kamaljohnson <kamaljohnson12345@gmail.com>
2021-07-15 20:00:50 +0530

    feat: Remove guest view for catalog

--- frappe_accounting_app/www/catalog/index.py
+++ frappe_accounting_app/www/catalog/index.py
@@ -1,6 +1,10 @@
 import frappe
+from frappe import _
 
 def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.throw(_("Log in to access this page."), frappe.PermissionError)
+
     context.company_name = frappe.get_all('Company')[0]['name']
     context.items = frappe.get_all('Item', fields=['image', 'name', 'standard_selling_rate'])
     return context
