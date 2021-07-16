2d30851b0122c0ac7b12f6ab8b0b0114221dcf92
kamaljohnson <kamaljohnson12345@gmail.com>
2021-07-16 07:53:33 +0530

    feat: Add add-to-cart and view-cart buttons in item view  - feat: Create python callback for add-to-cart button  - ui: Improve items page ui

--- frappe_accounting_app/frappe_accounting_app/doctype/item/item.py
+++ frappe_accounting_app/frappe_accounting_app/doctype/item/item.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2021, Kamal Johnson and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 class Item(WebsiteGenerator):
 	pass
+
+@frappe.whitelist(allow_guest=False)
+def add_item_to_cart(item_code):
+	print('adding item {} to cart'.format(item_code))
---
+++ frappe_accounting_app/frappe_accounting_app/doctype/item/templates/item.js
@@ -0,0 +1,12 @@
+function on_add_to_cart_click() {
+    frappe.call({
+        method: "frappe_accounting_app.frappe_accounting_app.doctype.item.item.add_item_to_cart",
+        args: {
+            "item_code": '{{ doc.item_code }}',
+        }
+    })
+}
+
+function on_view_cart_click() {
+    console.log('view cart clicked {{ doc.item_code}}')
+}
