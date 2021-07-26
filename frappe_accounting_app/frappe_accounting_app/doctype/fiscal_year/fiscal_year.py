1b286d8936d2d156d955d9c1abd3207b21b52b35
kamaljohnson <kamaljohnson12345@gmail.com>
2021-07-26 18:49:38 +0530

    feat: Add fiscal year validation before save

--- frappe_accounting_app/frappe_accounting_app/doctype/fiscal_year/fiscal_year.py
+++ frappe_accounting_app/frappe_accounting_app/doctype/fiscal_year/fiscal_year.py
@@ -1,8 +1,51 @@
 # Copyright (c) 2021, Kamal Johnson and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class FiscalYear(Document):
-	pass
+
+	def before_save(self):
+		"""
+		1. Check if from_date to to_date lies inside any exsisting fiscal years
+		2. Else proceed to save the fiscal year
+		"""
+
+		if self.from_date < self.to_date:
+			if not self.validate_overlaps():
+				frappe.throw(_('Fiscal year overlapping with existing one'))
+		else:
+			frappe.throw(_('from_date should be before to_date'))
+
+	def validate_overlaps(self):
+
+		overlapping_fiscal_years = frappe.get_all(
+			'Fiscal Year',
+			filters={
+				'from_date': ['<=', self.from_date],
+				'to_date': ['>=', self.from_date],
+				'name': ['!=', self.name]
+			}
+		)
+
+		overlapping_fiscal_years += frappe.get_all(
+			'Fiscal Year',
+			filters={
+				'from_date': ['<=', self.to_date],
+				'to_date': ['>=', self.to_date],
+				'name': ['!=', self.name]
+			}
+		)
+
+		overlapping_fiscal_years += frappe.get_all(
+			'Fiscal Year',
+			filters={
+				'from_date': ['>=', self.from_date],
+				'to_date': ['<=', self.to_date],
+				'name': ['!=', self.name]
+			}
+		)
+
+		return len(overlapping_fiscal_years) == 0
