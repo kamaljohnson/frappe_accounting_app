66269884edd0dad96f5f05c0e40b5bd083d2a99d
kamaljohnson <kamaljohnson12345@gmail.com>
2021-07-30 19:24:24 +0530

    feat: Add account_type to account doctype, auto set balance type

--- frappe_accounting_app/frappe_accounting_app/doctype/account/account.py
+++ frappe_accounting_app/frappe_accounting_app/doctype/account/account.py
@@ -1,8 +1,16 @@
 # Copyright (c) 2021, Kamal Johnson and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.utils.nestedset import NestedSet
 
 class Account(NestedSet):
-	pass
+
+	def before_save(self):
+		if self.account_type in ['Income', 'Liability']:
+			self.balance_type = 'Credit'
+		elif self.account_type in ['Asset', 'Expense']:
+			self.balance_type = 'Debit'
+		else:
+			frappe.throw(_('Account type not found'))
