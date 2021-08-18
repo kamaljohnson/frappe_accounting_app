325b4cd682c47a5251ac3f98734d94ed850adfa3
kamaljohnson <kamaljohnson12345@gmail.com>
2021-08-18 11:47:29 +0530

    feat: Add dynamic amount/grand_total update in purchase invocie

--- frappe_accounting_app/frappe_accounting_app/doctype/purchase_invoice/purchase_invoice.py
+++ frappe_accounting_app/frappe_accounting_app/doctype/purchase_invoice/purchase_invoice.py
@@ -10,19 +10,7 @@ from frappe import _
 class PurchaseInvoice(Document):
 
     def before_save(self):
-        """
-		1. Compute amount field for each purhcase invoice item
-		2. Calculate the grand_total
-		"""
-
-        grand_total = 0
-
-        for purchase_invoice_item in self.get('items'):
-            amount = flt(purchase_invoice_item.rate) * purchase_invoice_item.quantity
-            purchase_invoice_item.amount = amount
-            grand_total += amount
-
-        self.grand_total = grand_total
+        pass
 
     def on_submit(self):
         """
